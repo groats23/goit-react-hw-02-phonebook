193c194d26cf410bee17d0c3cdcf202ef79f2ff5
groats23 <121118901+groats23@users.noreply.github.com>
2023-07-09 13:38:25 +0200

    summary

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { FaTrashAlt } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import {
+  ContactItemWrapper,
+  ContactItemName,
+  ContactItemNum,
+  DeleteBtn,
+} from './ContactItem.styled';
+
+export default function ContactItem({ id, name, number, onDeleteContact }) {
+  return (
+    <ContactItemWrapper>
+      <ContactItemName>{name}</ContactItemName>
+      <ContactItemNum href={`tel:${number}`}>{number}</ContactItemNum>
+      <DeleteBtn type="button" onClick={() => onDeleteContact(id)}>
+        <IconContext.Provider value={{ size: '2em' }}>
+          <FaTrashAlt />
+        </IconContext.Provider>
+      </DeleteBtn>
+    </ContactItemWrapper>
+  );
+}
+
+ContactItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
